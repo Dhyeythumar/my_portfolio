00a30960bc6c4c3f12c876a46e0d0d214ec57f07
Dhyey Thumar <40618550+Dhyeythumar@users.noreply.github.com>
2020-05-08 10:00:31 +0530

    Update interaction.js

--- src/interaction.js
+++ src/interaction.js
@@ -55,28 +55,51 @@ window.addEventListener("resize", function() {
 
 
 jQuery(document).ready(function($) 
+{
+// Header fixed and Back to top button
+$(window).scroll(function() 
+{
+    if ($(this).scrollTop() > 100) 
     {
-        // Header fixed and Back to top button
-        $(window).scroll(function() 
-        {
-            if ($(this).scrollTop() > 100) 
-            {
-                $('.back-to-top').fadeIn('slow');
-            } 
-            else
-            {
-                $('.back-to-top').fadeOut('slow');
-            }
-        });
+	$('.back-to-top').fadeIn('slow');
+    } 
+    else
+    {
+	$('.back-to-top').fadeOut('slow');
+    }
+});
 
-        if ($(this).scrollTop() > 100) 
-        {
-            $('.back-to-top').fadeIn('slow');
-        }
+if ($(this).scrollTop() > 100) 
+{
+    $('.back-to-top').fadeIn('slow');
+}
 
-        $('.back-to-top').click(function() 
-        {
-            $('html, body').animate({scrollTop: 0}, 2000);
-            return false;
-        });
+$('.back-to-top').click(function() 
+{
+    $('html, body').animate({scrollTop: 0}, 2000);
+    return false;
+});
+});
+
+
+// Lazy Load for my image.
+window.addEventListener('load', function() {
+    // setTimeout to simulate the delay from a real page load
+    setTimeout(lazyLoad, 1000);
+});
+
+function lazyLoad() {
+    var card_image = document.getElementsByClassName('card-image')[0];
+
+    var content_image = card_image.querySelector('img');
+    var image_url = content_image.getAttribute('data-image-full');
+
+    // change the src of the content image to load the new high res photo
+    content_image.src = image_url;
+    
+    // listen for load event when the new photo is finished loading
+    content_image.addEventListener('load', function() {
+        // add a class to remove the blur filter to smoothly transition the image change
+        card_image.className = card_image.className + ' is-loaded';
     });
+}
